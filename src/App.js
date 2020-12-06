52c5e444e5489e5135df3aa3190f7eca0b2c9f38
Akram Khalid <akramkhalid12@gmail.com>
2020-12-06 16:25:02 -0600

    init starter

--- src/App.js
+++ src/App.js
@@ -1,69 +1,16 @@
-import React, { Suspense, useState } from "react";
-//Three
-import { Canvas, useThree } from "react-three-fiber";
-import { Loader, OrbitControls, softShadows } from "@react-three/drei";
-import { useSpring } from "react-spring";
-import Lights from "./components/Three/lights";
-import Floor from "./components/Three/floor";
+import React from "react";
 //Styles
 import "./assets/styles/App.scss";
-//Model
-import Model from "./components/Three/chest";
-// Chest UI Component
-import ChestModal from "./components/chestModal";
-
-// Initiate softShadows
-softShadows();
-
-// Create the zoom effect once the page has loaded
-const ZoomWithOrbital = () => {
-  const { gl, camera } = useThree();
-  useSpring({
-    from: {
-      z: 30,
-    },
-    x: -5,
-    y: 4,
-    z: 4,
-    // React Springs onFrame
-    onFrame: ({ x, y, z }) => {
-      camera.position.x = x;
-      camera.position.y = y;
-      camera.position.z = z;
-    },
-  });
-  return (
-    // Oribital controls via drei
-    <OrbitControls
-      enableZoom={false}
-      enablePan={false}
-      target={[0, 0, 0]}
-      args={[camera, gl.domElement]}
-    />
-  );
-};
 
 const App = () => {
-  // State if chest is open
-  const [open, setOpen] = useState(false);
   return (
-    <>
-      <Canvas
-        colorManagement
-        shadowMap
-        camera={{ position: [-5, 4, 4], fov: 40 }}>
-        <Lights />
-        <Suspense fallback={null}>
-          <Model open={open} setOpen={setOpen} />
-          <Floor />
-          <ZoomWithOrbital />
-        </Suspense>
-      </Canvas>
-      {/* Loading bar */}
-      <Loader />
-      {/* Chest Modal UI */}
-      <ChestModal open={open} setOpen={setOpen} />
-    </>
+    <div className='container'>
+      <h3>Hey everyone! Lets learn about React Three Fiber :)</h3>
+      <p>
+        Dont forget to subscribe
+        <a href='https://youtube.com/c/wrongakram'>@wrongakram</a>
+      </p>
+    </div>
   );
 };
 
